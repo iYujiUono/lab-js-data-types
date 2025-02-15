124b11daa613432a4143fd8cdff82bd626cda09f
Yuji Uono <yuuono.1998@gmail.com>
2025-02-15 16:45:41 +0100

    dataType complete

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4} `;
 // Concatenate the string variables into one new string
-
+console.log(tongueTwister);
 
 // Print out the concatenated string
 
@@ -21,8 +22,19 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+//javA
+
+
+const upperJV = part1.slice(0,3)+ part1.slice(-1).toUpperCase();
+console.log(upperJV);
+
+const upperSt = part2.slice(0,6) + part2.slice(-1).toUpperCase();
+ console.log(upperSt);
+
+ const result = upperJV + upperSt;
+ console.log(result);
 
 // Print the cameLtaiL-formatted string
 
@@ -35,10 +47,12 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const tipAmount = (billTotal * 0.15);
 
-
+const total = billTotal + tipAmount;
 // Print out the tipAmount
 
+console.log(`The bill was ${billTotal}, plus ${tipAmount} and the total is ${total}`);
 
 
 
@@ -47,10 +61,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+let randomNumber = Math.floor((Math.random() * 10) + 1);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +76,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+// false
 const expression2 = a || b;
-
+// true
 const expression3 = !a && b;
-
+// true 
 const expression4 = !(a && b);
-
+// true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
